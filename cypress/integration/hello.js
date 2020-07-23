428b5dda89564e672b8836820960f743c2de65c4
Artem Sapegin <artem@sapegin.ru>
2020-07-23 16:55:09 +0200

    Add description()s

--- cypress/integration/hello.js
+++ cypress/integration/hello.js
@@ -1,14 +1,16 @@
-it('hello world', () => {
-  cy.visit('/');
-  cy.findByText(/pizza/i).should('be.visible');
-});
+describe('Basic tests', () => {
+  it('hello world', () => {
+    cy.visit('/');
+    cy.findByText(/pizza/i).should('be.visible');
+  });
 
-it('navigates to another page', () => {
-  cy.visit('/');
+  it('navigates to another page', () => {
+    cy.visit('/');
 
-  cy.log('Opening the pizza page');
-  cy.findByRole('link', { name: /remotepizza/i }).click();
+    cy.log('Opening the pizza page');
+    cy.findByRole('link', { name: /remotepizza/i }).click();
 
-  cy.log('We are on the pizza page');
-  cy.findByRole('heading', { name: /pizza/i }).should('be.visible');
+    cy.log('We are on the pizza page');
+    cy.findByRole('heading', { name: /pizza/i }).should('be.visible');
+  });
 });
--- cypress/integration/remotePizza.js
+++ cypress/integration/remotePizza.js
@@ -1,43 +1,45 @@
-const ingredients = ['bacon', 'tomato', 'mozzarella', 'pineapples'];
+describe('Async tests', () => {
+  const ingredients = ['bacon', 'tomato', 'mozzarella', 'pineapples'];
 
-it('load ingredients asynchronously', () => {
-  cy.visit('/remote-pizza');
+  it('load ingredients asynchronously', () => {
+    cy.visit('/remote-pizza');
 
-  cy.log('Ingredients list is not visible');
-  cy.findByText(ingredients[0]).should('not.be.visible');
+    cy.log('Ingredients list is not visible');
+    cy.findByText(ingredients[0]).should('not.be.visible');
 
-  cy.log('Load ingredients');
-  cy.findByRole('button', { name: /cook/i }).click();
+    cy.log('Load ingredients');
+    cy.findByRole('button', { name: /cook/i }).click();
 
-  cy.log('All ingredients appear on the screen');
-  for (const ingredient of ingredients) {
-    cy.findByText(ingredient).should('be.visible');
-  }
+    cy.log('All ingredients appear on the screen');
+    for (const ingredient of ingredients) {
+      cy.findByText(ingredient).should('be.visible');
+    }
 
-  cy.log('The button is not clickable anymore');
-  cy.findByRole('button', { name: /cook/i }).should('be.disabled');
-});
-
-it('shows an error message', () => {
-  cy.visit('/remote-pizza');
-
-  cy.window().then((window) => {
-    // Reference global instances set in src/browser.js
-    const { worker, rest } = window.msw;
-    worker.use(
-      rest.get('https://httpbin.org/anything', (req, res, ctx) => {
-        return res.once(ctx.status(500));
-      })
-    );
+    cy.log('The button is not clickable anymore');
+    cy.findByRole('button', { name: /cook/i }).should('be.disabled');
   });
 
-  cy.log('Ingredients list is not visible');
-  cy.findByText(ingredients[0]).should('not.be.visible');
+  it('shows an error message', () => {
+    cy.visit('/remote-pizza');
 
-  cy.log('Load ingredients');
-  cy.findByRole('button', { name: /cook/i }).click();
+    cy.window().then((window) => {
+      // Reference global instances set in src/browser.js
+      const { worker, rest } = window.msw;
+      worker.use(
+        rest.get('https://httpbin.org/anything', (req, res, ctx) => {
+          return res.once(ctx.status(500));
+        })
+      );
+    });
 
-  cy.log('Ingredients list is still not visible and error message appears');
-  cy.findByText(ingredients[0]).should('not.be.visible');
-  cy.findByText(/something went wrong/i).should('be.visible');
+    cy.log('Ingredients list is not visible');
+    cy.findByText(ingredients[0]).should('not.be.visible');
+
+    cy.log('Load ingredients');
+    cy.findByRole('button', { name: /cook/i }).click();
+
+    cy.log('Ingredients list is still not visible and error message appears');
+    cy.findByText(ingredients[0]).should('not.be.visible');
+    cy.findByText(/something went wrong/i).should('be.visible');
+  });
 });
