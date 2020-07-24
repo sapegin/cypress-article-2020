80fe94366071324b90b0f5d7d412ce457e6a8b0a
Artem Sapegin <artem@sapegin.ru>
2020-07-24 12:18:13 +0200

    Another test

--- cypress/integration/profile.js
+++ cypress/integration/profile.js
@@ -15,4 +15,19 @@ describe('User profile', () => {
       'be.visible'
     );
   });
+
+  it('should stay on the profile page after cancelling profile deletion', () => {
+    cy.visit('/profile');
+
+    cy.log('Attempting to delete profile');
+    cy.findByRole('button', { name: /delete profile/i }).click();
+
+    cy.log('Confirming deletion');
+    cy.findByTestId('delete-profile-modal').within(() => {
+      cy.findByRole('button', { name: /cancel/i }).click();
+    });
+
+    cy.log('We are back on the profile page');
+    cy.findByRole('heading', { name: /^your profile$/i }).should('be.visible');
+  });
 });
